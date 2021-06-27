25c045477ea1c3e64d43ff17a4d688f93169c519
Jackson Souza <jacksonrodrigues4049@gmail.com>
2021-06-27 03:56:13 -0300

    feat: Adiciona view app para crawler

--- crawler/views.py
+++ crawler/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+import json
+from crawler.jobs.scrapper import Scrapper
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view(['POST'])
+def app(request):
+    body = json.loads(request.body)
+    scrapper = Scrapper(city=body['city'])
+    response = scrapper.run()
+    return Response(
+        response
+    )
